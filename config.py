75c46ca23686e41a0eb32fcd61e11c986428b724
jesuslzow1 <jesuslz9721@gmail.com>
2020-06-16 22:39:12 -0500

    Se añadio la parte de circuitos

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-an-optics-user'
     TEMPLATES_AUTO_RELOAD = True
-    DEBUG = True
+    #DEBUG = True
